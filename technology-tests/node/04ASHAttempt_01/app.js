243cace93f8b49d32778cf12df46ce9bc15f54e7
Curran Kelleher <curran.kelleher@gmail.com>
2011-09-24 13:55:51 -0400

    updated markdown doc

--- technology-tests/node/04ASHAttempt_01/app.js
+++ technology-tests/node/04ASHAttempt_01/app.js
@@ -15,18 +15,3 @@ function handler (req, res) {
     res.end(content, 'utf-8');
   });
 }
-
-var sockets = [];
-io.sockets.on('connection', function (socket) {	
-  sockets.push(socket);
-  socket.on('disconnect', function (){
-    var i = sockets.indexOf(socket);
-    sockets.splice(i, 1);
-  });
-  socket.on('commitTransaction', function (data) {
-    for(var i = 0; i < sockets.length; i++)
-      if(sockets[i] != socket)
-	sockets[i].emit('executeTransaction', data);
-    console.log(data);
-  });
-});
