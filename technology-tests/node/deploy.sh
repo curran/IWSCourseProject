a0ac8d4c974799225ccd0fc63fe8c5e817597556
Curran Kelleher <curran.kelleher@gmail.com>
2011-09-24 17:21:53 -0300

    fixed syntax error

--- technology-tests/node/deploy.sh
+++ technology-tests/node/deploy.sh
@@ -1,4 +1,9 @@
 git pull
 killall node
-cd 03SynchronizedCircles/; nohup node app.js &; cd ../
-cd 05ASHAttempt_02/; nohup node app.js &; cd ../
+cd 03SynchronizedCircles/
+nohup node app.js &
+cd ../
+
+cd 05ASHAttempt_02/
+nohup node app.js &
+cd ../
