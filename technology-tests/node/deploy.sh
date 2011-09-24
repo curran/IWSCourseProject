618a591c3fffe4fd378fdb77513246d51f035e47
Curran Kelleher <curran.kelleher@gmail.com>
2011-09-24 17:04:34 -0400

    modified deploy script

--- technology-tests/node/deploy.sh
+++ technology-tests/node/deploy.sh
@@ -1,9 +1,13 @@
 git pull
 killall node
 export NODE_PATH=`pwd`
+
+# running on port 8000
 cd 03SynchronizedCircles
 nohup node app.js &
 cd ../
+
+# running on port 8080
 cd 05ASHAttempt_02
 nohup node app.js &
 cd ../
