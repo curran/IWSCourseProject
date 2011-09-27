382e6536905a106c595cba60133028b7fe25c651
Curran Kelleher <curran.kelleher@gmail.com>
2011-09-27 10:36:51 -0400

    got ASH graphical test working, client and server

--- technology-tests/node/06ASHAttempt_03/ash.js
+++ technology-tests/node/06ASHAttempt_03/ash.js
@@ -1,4 +1,31 @@
 var ASH = (function() {
+  var socket = io.connect('/');
+  socket.on('executeTransaction', function (data) {
+    console.log("receiving ("+data+")");
+    var tokens = data.split(" ");
+    actionSource = 'server';
+    if(tokens[0] == 's')
+    //commitTransaction("s "+resource+" "+property+" "+value);
+      ASH.set(tokens[1],tokens[2],tokens[3]);
+    else if(tokens[0] == 'us')
+      ASH.set(tokens[1],tokens[2]);
+    actionSource = 'client';
+  });
+  function commitTransaction(data){
+    console.log("sending ("+data+")");
+    socket.emit('commitTransaction', data);
+  }
+/*
+  //send updates at most every Xms
+  var millisec = 50;
+  setInterval(function(){
+      if(positionChanged){
+        console.log("sending ("+x+","+y+")");
+        send(x,y);
+        positionChanged = false;
+      }
+    },millisec);
+*/
   // private members
   
   var resourceIdCounter = 0;
@@ -9,9 +36,7 @@ var ASH = (function() {
   
   var resources = {}; // keys = resource ids, values = objects with set(property,value) and unset(property)
 
-  function privateMethod () {
-    // ...
-  }
+  var actionSource = 'client'; // one of 'client' or 'server'
 
   return { // public interface
     registerPlugin: function (plugin) {
@@ -21,17 +46,24 @@ var ASH = (function() {
       return (resourceIdCounter++).toString();
     },
     set: function (resource, property, value) {
-      if(property == ASH.TYPE){
-        var type = value;
-        resourceTypes[resource] = type;
-        resources[resource] = plugins[type].create(resource);
+      if(actionSource == 'client')
+        commitTransaction("s "+resource+" "+property+" "+value);
+      else if(actionSource == 'server'){
+        if(property == ASH.TYPE){
+          var type = value;
+          resourceTypes[resource] = type;
+          resources[resource] = plugins[type].create(resource);
+        }
+        else
+          resources[resource].set(property,value);
       }
-      else
-        resources[resource].set(property,value);
     },
     unset: function (resource, property) {
-      if(property == ASH.TYPE){
-        plugins[resourceTypes[resource]].delete(resource);
+      if(actionSource == 'client')
+        commitTransaction("us "+resource+" "+property);
+      else if(actionSource == 'server'){
+        if(property == ASH.TYPE)
+          plugins[resourceTypes[resource]].delete(resource);
       }
     },
     TYPE : "type"
